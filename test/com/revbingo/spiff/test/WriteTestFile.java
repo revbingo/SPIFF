bf8bfa96fdf44137adfe326576a8e9d3956779df
Mark Piper <revbingo@gmail.com>
2010-10-18 22:28:37 +0100

    Use GPLv3 license

--- test/com/revbingo/spiff/test/WriteTestFile.java
+++ test/com/revbingo/spiff/test/WriteTestFile.java
@@ -1,9 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2010 Mark Piper.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * This file is part of SPIFF.
+ * 
+ * SPIFF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SPIFF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SPIFF.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package com.revbingo.spiff.test;
 
