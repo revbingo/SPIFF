4eaca74f04ec3106c17ca1979b7b274e37b8e6bf
Mark Piper <revbingo@gmail.com>
2010-10-18 22:17:35 +0100

    Add GPL license

--- test/com/revbingo/spiff/TestCaseBinaryParser.java
+++ test/com/revbingo/spiff/TestCaseBinaryParser.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Mark Piper.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ ******************************************************************************/
 package com.revbingo.spiff;
 
 import static org.hamcrest.CoreMatchers.is;
