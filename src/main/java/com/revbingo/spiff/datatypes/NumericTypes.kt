ea867f5d0db21dd6900e80bfaf81d74e6fd915d7
Mark Piper <mark.piper@wds.co>
2016-02-25 09:34:44 +0000

    MP: More simplification of number types

--- src/main/java/com/revbingo/spiff/datatypes/NumericTypes.kt
+++ src/main/java/com/revbingo/spiff/datatypes/NumericTypes.kt
@@ -20,94 +20,70 @@ import java.nio.ByteBuffer
 
 import com.revbingo.spiff.evaluator.Evaluator
 
-abstract class NumberType : Datatype() {
+abstract class NumberType(val type: Class<out Any>,
+                          val bufferFunc: (ByteBuffer) -> Number) : Datatype() {
 
     var literalExpr: String? = null
 
-    fun verifyNumber(numberToCheck: Number, type: Class<out Any>, evaluator: Evaluator): Unit {
-        if(literalExpr != null) {
-            val expressionResult = evaluator.evaluate(literalExpr, type as Class<Any>)
-            if(numberToCheck != expressionResult) throw ExecutionException("Value $numberToCheck did not match expected value $literalExpr")
-        }
-    }
-}
-
-abstract class SimpleNumberType(val type: Class<out Any>,
-                                val bufferFunc: (ByteBuffer) -> Number): NumberType() {
-
     override fun evaluate(buffer: ByteBuffer, evaluator: Evaluator): Any {
         val value = bufferFunc(buffer)
-        verifyNumber(value, type, evaluator)
+        if(literalExpr != null) {
+            val expressionResult = evaluator.evaluate(literalExpr, type as Class<Any>)
+            if(value != expressionResult) throw ExecutionException("Value $value did not match expected value $literalExpr")
+        }
         return value
     }
 }
 
-class ByteInstruction: SimpleNumberType(Byte::class.java, { it.get() })
+class ByteInstruction: NumberType(Byte::class.java, { it.get() })
 
-class ShortInstruction: SimpleNumberType(Short::class.java, { it.short })
+class ShortInstruction: NumberType(Short::class.java, { it.short })
 
-class IntegerInstruction: SimpleNumberType(Int::class.java, { it.int })
+class IntegerInstruction: NumberType(Int::class.java, { it.int })
 
-class LongInstruction: SimpleNumberType(Long::class.java, { it.long })
+class LongInstruction: NumberType(Long::class.java, { it.long })
 
-class DoubleInstruction : SimpleNumberType(Double::class.java, { it.double })
+class DoubleInstruction : NumberType(Double::class.java, { it.double })
 
-class FloatInstruction: SimpleNumberType(Float::class.java, { it.float })
+class FloatInstruction: NumberType(Float::class.java, { it.float })
 
 /*
 * Unsigned numbers are represented by widening to the next widest type i.e. unsigned bytes are shorts,
 * unsigned shorts are ints, unsigned ints are longs, unsigned longs are not supported
 * */
-fun ByteArray.convertToInts(): IntArray = this.map { byte -> byte.toInt() and 0xFF }.toIntArray()
+fun ByteArray.convertToUnsignedInts(): IntArray = this.map { byte -> byte.toInt() and 0xFF }.toIntArray()
 
-class UnsignedByteInstruction: NumberType() {
+class UnsignedByteInstruction: NumberType(Short::class.java, {
+    val bytes = ByteArray(1)
+    it.get(bytes)
 
-    override fun evaluate(buffer: ByteBuffer, evaluator: Evaluator): Any {
-        val bytes = ByteArray(1)
-        buffer.get(bytes)
+    bytes.convertToUnsignedInts()[0].toShort()
+})
 
-        val s = bytes.convertToInts()[0].toShort()
+class UnsignedIntegerInstruction: NumberType(Long::class.java, {
+    val bytes = ByteArray(4)
+    val signedInt = it.int
 
-        this.verifyNumber(s, Short::class.java, evaluator)
-        return s
-    }
-}
+    bytes[0] = (signedInt shr 24).toByte()
+    bytes[1] = (signedInt shr 16).toByte()
+    bytes[2] = (signedInt shr 8).toByte()
+    bytes[3] = signedInt.toByte()
 
-class UnsignedIntegerInstruction: NumberType() {
+    val ubytes = bytes.convertToUnsignedInts()
 
-    override fun evaluate(buffer: ByteBuffer, evaluator: Evaluator): Any {
-        val bytes = ByteArray(4)
-        val signedInt = buffer.int
+    ((ubytes[0] shl 24)
+            or (ubytes[1] shl 16)
+            or (ubytes[2] shl 8)
+            or (ubytes[3])).toLong()
+})
 
-        bytes[0] = (signedInt shr 24).toByte()
-        bytes[1] = (signedInt shr 16).toByte()
-        bytes[2] = (signedInt shr 8).toByte()
-        bytes[3] = signedInt.toByte()
 
-        val ubytes = bytes.convertToInts()
+class UnsignedShortInstruction: NumberType(Int::class.java, {
+    val bytesAsInts = IntArray(2)
+    val signedShortAsInt = it.short.toInt()
 
-        val long = ((ubytes[0] shl 24)
-                 or (ubytes[1] shl 16)
-                 or (ubytes[2] shl 8)
-                 or (ubytes[3])).toLong()
+    bytesAsInts[0] = (signedShortAsInt shr 8) and 0xFF
+    bytesAsInts[1] = signedShortAsInt and 0xFF
 
-        verifyNumber(long, Long::class.java, evaluator)
-        return long
-    }
-}
-
-class UnsignedShortInstruction: NumberType() {
-
-    override fun evaluate(buffer: ByteBuffer, evaluator: Evaluator): Any {
-        val bytesAsInts = IntArray(2)
-        val signedShortAsInt = buffer.short.toInt()
-
-        bytesAsInts[0] = (signedShortAsInt shr 8) and 0xFF
-        bytesAsInts[1] = signedShortAsInt and 0xFF
-
-        val i = (bytesAsInts[0] shl 8) or bytesAsInts[1]
-
-        verifyNumber(i, Int::class.java, evaluator)
-        return i
-    }
-}
+    (bytesAsInts[0] shl 8) or bytesAsInts[1]
+})
